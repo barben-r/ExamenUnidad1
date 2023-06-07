b38ee169237cd44327d2ae58248defca16f71788
barben-r <107881200+barben-r@users.noreply.github.com>
2023-06-06 20:44:40 -0600

    Update exam

--- ExOrdU2SalgadoArmijoRubenBaruc/js1/tele.js
+++ ExOrdU2SalgadoArmijoRubenBaruc/js1/tele.js
@@ -9,7 +9,7 @@ const btnPower = document.getElementById("power");
 const btnVolumeUp = document.getElementById("volumeUp");
 const btnVolumeDown = document.getElementById("volumeDown");
 const btnChanelUp = document.getElementById("chanelUp");
-const btnChaneDown = document.getElementById("chanelDown");
+const btnChanelDown = document.getElementById("chanelDown");
 const btnMute = document.getElementById("mute");
 const chanel = document.getElementById("contChanel");
 const volume = document.getElementById("contVolume");
@@ -19,6 +19,9 @@ btnPower.addEventListener("click", () => {
   if (!tv.power) {
     tv.power = true;
     tv.volume = 3;
+    tv.chanel = 7;
+    volume.innerHTML = `${tv.volume}`;
+    chanel.innerHTML = `${tv.chanel}`;
     btnPower.classList.remove("bg-danger");
     btnPower.classList.add("bg-success");
   } else {
@@ -58,12 +61,26 @@ btnChanelUp.addEventListener("click", () => {
   }
 });
 
-btnChaneDown.addEventListener("click", () => {
+btnChanelDown.addEventListener("click", () => {
   chanel.innerHTML = `${tv.chanel}`;
   if (tv.chanel >= 1) {
     tv.chanel--;
   } else {
     console.log("canal minimo");
     tv.chanel = 1;
+  }
+});
+
+btnMute.addEventListener("click", () => {
+  if (!tv.mute) {
+    tv.mute = true;
+    tv.volume = 0;
+    volume.innerHTML = `${tv.volume}`;
+    btnMute.classList.remove("bg-primary");
+    btnMute.classList.add("bg-success");
+  } else {
+    tv.mute = false;
+    btnMute.classList.remove("bg-success");
+    btnMute.classList.add("bg-primary");
   }
 });
